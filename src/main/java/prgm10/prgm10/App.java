ca2fcfb61e30b086bf024ea225109866a07f8491
Your Name <you@example.com>
2025-05-28 04:12:57 +0530

    azure pipeline example

---
+++ src/main/java/prgm10/prgm10/App.java
@@ -0,0 +1,13 @@
+package prgm10.prgm10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
